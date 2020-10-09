2e8c57dcf08755a5d0917105b3909d9586c0a24a
Jarret Oldham <jdoldham@breakawayanalytics.co>
2020-10-09 13:23:33 -0400

    adding format select

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
 import Slider from 'rc-slider'; 
+import Select from '@material-ui/core/Select'; 
+import MenuItem from '@material-ui/core/MenuItem'; 
 import 'rc-slider/assets/index.css'; 
 import './Navbar.css'; 
 
 export default class Navbar extends Component {
+  constructor(props) {
+    super(props); 
+
+    this.state = { format: 'hex'}; 
+
+    this.handleChange = this.handleChange.bind(this); 
+  }
+
+  handleChange(e) { 
+    this.setState({ format: e.target.value }); 
+
+    this.props.handleChange(e.target.value); 
+  }
+
   render() {
     const { level, changeLevel } = this.props; 
+    const { format } = this.state;  
     return (
       <header className="Navbar">
         <div className="logo">
@@ -22,7 +39,13 @@ export default class Navbar extends Component {
             /> 
           </div>
         </div>
-        
+        <div className="select-container">
+          <Select value={format} onChange={this.handleChange}>
+            <MenuItem value="hex">HEX - #fffff</MenuItem>
+            <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem>
+          </Select>
+        </div>
       </header>
     )
   }
