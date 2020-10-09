1efeb1d25361cf22d7f642a1fe4f09ef8e5179e7
Jarret Oldham <jdoldham@breakawayanalytics.co>
2020-10-09 14:03:56 -0400

    adding routing

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import './App.css';
 import { generatePalette } from './colorHelpers';
 import Palette from './Palette'; 
 import seedColors from './seedColors'; 
+import { Route, Switch } from 'react-router-dom'; 
 
 class App extends Component {
+  findPalette(id) {
+    return seedColors.find(palette => {
+      return palette.id === id; 
+    }); 
+  }
+
   render() {
     return (
-      <div className="App">
-        <Palette palette={generatePalette(seedColors[4])}/> 
-      </div>
+      <Switch>
+        <Route exact 
+          path='/' 
+          render={() => <h1>LIST GOES HERE</h1>}/> 
+        <Route exact 
+          path='/palette/:id' 
+          render={(routeProps) => <Palette palette={
+            generatePalette(this.findPalette(routeProps.match.params.id))}/>
+          }/> 
+      </Switch>
     );
   } 
 }
